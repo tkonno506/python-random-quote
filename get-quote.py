39d758f9c8274067388256d3ba68302f380f3fa1
toshiharu konno <tkonno@mx1.wiseman.co.jp>
2020-07-17 21:48:37 +0900

    test

--- get-quote.py
+++ get-quote.py
@@ -1,10 +1,13 @@
+import random
+
 def main():
   print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-
-  print(quotes[12])
+  last = len(quotes) - 1
+  rnd = random.randint(0, last)
+  print(quotes[rnd])
 if __name__== "__main__":
   main()
